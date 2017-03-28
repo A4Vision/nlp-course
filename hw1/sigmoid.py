a5a37867f17ce9cc8cf44843085cfaf47104757d
Assaf <assafyifrach@mail.tau.ac.il>
2017-03-28 17:43:53 +0300

    Added sigmoid impl.

--- hw1/sigmoid.py
+++ hw1/sigmoid.py
@@ -15,7 +15,7 @@ def sigmoid(x):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    s = 1. / (1. + np.exp(-x))
     ### END YOUR CODE
 
     return s
@@ -35,7 +35,7 @@ def sigmoid_grad(s):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    ds = (s * (1 - s))
     ### END YOUR CODE
 
     return ds
@@ -72,10 +72,38 @@ def test_sigmoid():
     """
     print "Running your tests..."
     ### YOUR CODE HERE
-    raise NotImplementedError
+    x1 = np.array([0])
+    f1 = sigmoid(x1)
+    g1 = sigmoid_grad(f1)
+    print f1
+    f1_ans = np.array([[0.5]])
+    assert np.allclose(f1, f1_ans, rtol=1e-05, atol=1e-06)
+    print g1
+    g1_ans = np.array([[0.25]])
+    assert np.allclose(g1, g1_ans, rtol=1e-05, atol=1e-06)
+
+    # x2 = np.array([0, 0])
+    # f2 = sigmoid(x2)
+    # g2 = sigmoid_grad(f2)
+    # print f2
+    # f2_ans = np.array([[0.5, 0.5]])
+    # assert np.allclose(f2, f2_ans, rtol=1e-05, atol=1e-06)
+    # print g2
+    # g2_ans = np.array([[0.25, 0.25]])
+    # assert np.allclose(g2, g2_ans, rtol=1e-05, atol=1e-06)
+
+    x3 = np.array([[0, 0], [1, 1]])
+    f3 = sigmoid(x3)
+    g3 = sigmoid_grad(f3)
+    print f3
+    f3_ans = np.array([[0.5, 0.5], [0.73105858, 0.73105858]])
+    assert np.allclose(f3, f3_ans, rtol=1e-05, atol=1e-06)
+    print g3
+    g3_ans = np.array([[0.25, 0.25], [0.19661193, 0.19661193]])
+    assert np.allclose(g3, g3_ans, rtol=1e-05, atol=1e-06)
     ### END YOUR CODE
 
 
 if __name__ == "__main__":
-    test_sigmoid_basic();
+    test_sigmoid_basic()
     test_sigmoid()
