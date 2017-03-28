d127e96de12a78b54990868ddfd12f1ca31bfc1b
Assaf <assafyifrach@mail.tau.ac.il>
2017-03-28 20:34:05 +0300

    Finished knn

--- hw1/knn.py
+++ hw1/knn.py
@@ -16,7 +16,10 @@ def knn(vector, matrix, k=10):
     nearest_idx = []
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    norm_of_matrix = np.linalg.norm(matrix, axis=1)
+    similarity = np.dot(matrix, vector) / norm_of_matrix
+    nearest_idx = np.argpartition(similarity, -k)[-k:]
+    nearest_idx = nearest_idx[np.argsort(similarity[nearest_idx])][::-1]
     ### END YOUR CODE
     return nearest_idx
 
@@ -29,8 +32,16 @@ def test_knn():
     """
     print "Running your tests..."
     ### YOUR CODE HERE
-    raise NotImplementedError
+    nearest_idx_1 = knn(np.array([1, 2, 3]), np.array([[1, 2, 2], [1, 2, 3], [0, 0, 20], [5, 0, 5]]), 3)
+    assert np.allclose(nearest_idx_1, [1, 0, 2])
+    nearest_idx_2 = knn(np.array([1, 0, 0]), np.array([[1, 0, 0], [0, 1.02, 0], [-2, 0, 0], [-1.01, 0, 0], [-6, 0, 3]]),
+                        4)
+    assert np.allclose(nearest_idx_2, [0, 1, 4, 3])
     ### END YOUR CODE
+
+
+def cosine_sim(a, b):
+    np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
 if __name__ == "__main__":
     test_knn()
