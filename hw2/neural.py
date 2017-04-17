Fixed the gradient calculations + added my test

hw2/neural.py
@@ -71,10 +71,10 @@ def forward_backward_prop(data, labels, params, dimensions):
     ### END YOUR CODE
 
     ### YOUR CODE HERE: backward propagation
-    gradb2 = y_hat - labels
-    gradW2 = np.dot(h.T, gradb2)
-    gradb1 = np.dot(gradb2, W2.T) * sigmoid_grad(h)
-    gradW1 = np.dot(data.T, gradb1)
+    gradb2 = np.sum(y_hat - labels, axis=0)
+    gradW2 = np.dot(h.T, y_hat - labels)
+    gradb1 = np.sum(np.dot(y_hat - labels, W2.T) * sigmoid_grad(h), axis=0)
+    gradW1 = np.dot(data.T, np.dot(y_hat - labels, W2.T) * sigmoid_grad(h))
     ### END YOUR CODE
 
     ### Stack gradients (do not modify)
@@ -114,7 +114,18 @@ def your_sanity_checks():
     """
     print "Running your sanity checks..."
     ### YOUR CODE HERE
-#    raise NotImplementedError
+
+
+    N = 2
+    dimensions = [15, 1, 4]
+    data = np.random.randn(N, dimensions[0])  # each row will be a datum
+    labels = np.zeros((N, dimensions[2]))
+    for i in xrange(N):
+        labels[i, random.randint(0, dimensions[2] - 1)] = 1
+    params = np.random.randn((dimensions[0] + 1) * dimensions[1] + (
+        dimensions[1] + 1) * dimensions[2], )
+    gradcheck_naive(lambda params:
+        forward_backward_prop(data, labels, params, dimensions), params)
     ### END YOUR CODE
 
 